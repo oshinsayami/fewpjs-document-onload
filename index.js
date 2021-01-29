8eb1b10a1cb99cac18846ad88a84930563766e21
Simran <mdrsimran@gmail.com>
2021-01-28 22:55:57 -0600

    commit tree d967e939d13e30cede4e65b64a257598839b1be2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM();
+  }); 
+
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  ); 
+
+  function updateDOM(){
+      document.getElementById("text").innerHTML="This is really cool!";
+  }
